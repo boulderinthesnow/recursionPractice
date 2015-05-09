40ab792ca3b2576aed550dffea487679eebfd8ee
Matthew Williams <mwilliamsnov6@gmail.com>
2015-05-09 12:23:30 -0700

    added more files, added jasmine test suite

--- rec99Bottles.js
+++ rec99Bottles.js
@@ -1,20 +1,27 @@
-//Define a function that takes an argument n and returns 
-//the lyrics to 99 bottles of beer on the wall, starting 
-//with that number n. For example, singSong(99) would 
-//print out the lyrics to 99 Bottles of Beer on the Wall.
+//module.exports = {
 
-var recBottles = function (startBottles) {
-	if (startBottles === 0) {
-		return "You're drunk, go home"
-	}
-if (startBottles > 0)
-	console.log (startBottles + " bottles of beer on the wall");
-	console.log (startBottles + " bottles of beer");
-	console.log ("take one down, pass it around");
-	console.log (startBottles-1  + " bottles of beer on the wall! \n");
-	return recBottles(startBottles-1)
+	//Define a function that takes an argument n and returns 
+	//the lyrics to 99 bottles of beer on the wall, starting 
+	//with that number n. For example, singSong(99) would 
+	//print out the lyrics to 99 Bottles of Beer on the Wall.
 
-}
+	var recBottles = function (startBottles) { //99 bottles
+		newArr = [];
+		if (startBottles === 0) {
+			newArr.push("YOUR MOM");
+			return newArr;
+		}
+	if (startBottles > 0)
+//		console.log (startBottles, "starbottles")
+//		newArr.push("bottles of beer")
+		console.log(startBottles + " bottles of beer on the wall");
+		console.log(startBottles + " bottles of beer");
+		console.log("take one down, pass it around");
+		console.log(startBottles-1  + " bottles of beer on the wall! \n");
+		return recBottles(startBottles-1)
 
-console.log(recBottles(999));
+	} // end of it all
 
+	console.log(recBottles(10));
+
+// }
---
+++ spec/spec.js
@@ -0,0 +1,6 @@
+var bottles = require("../rec99Bottles.js");
+var sumRange = require("../sumOfRange.js");
+
+describe("recursive tests", function(){
+	it()
+})
